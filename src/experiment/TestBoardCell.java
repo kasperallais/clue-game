270798c78150e374cf6cebf02f0c79d82ef9270f
kasperallais <kasper.allais@gmail.com>
2024-10-07 10:45:18 -0600

    Filled in TestBoardCell class with requirements

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,5 +1,42 @@
 package experiment;
 
-public class TestBoardCell {
+import java.util.HashSet;
+import java.util.Set;
 
+public class TestBoardCell {
+    private int row;
+    private int col;
+    private boolean isRoom;
+    private boolean isOccupied;
+    private Set<TestBoardCell> adjList;
+
+    public TestBoardCell(int row, int col) {
+        this.row = row;
+        this.col = col;
+        this.adjList = new HashSet<>();
+    }
+
+    public void addAdjacency(TestBoardCell cell) {
+        adjList.add(cell);
+    }
+
+    public Set<TestBoardCell> getAdjList() {
+        return adjList;
+    }
+
+    public void setRoom(boolean isRoom) {
+        this.isRoom = isRoom;
+    }
+
+    public boolean isRoom() {
+        return isRoom;
+    }
+
+    public void setOccupied(boolean isOccupied) {
+        this.isOccupied = isOccupied;
+    }
+
+    public boolean getOccupied() {
+        return isOccupied;
+    }
 }
